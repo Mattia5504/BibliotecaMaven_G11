71e1e3f9d2323d15177159d7af1876bb96ac9515
JoJo <jonathanpunzo3@gmail.com>
2025-11-26 17:36:48 +0100

    Bozza abbastanza completa di Prestito

--- src/main/java/it/unisa/biblioteca/model/Prestito.java
+++ src/main/java/it/unisa/biblioteca/model/Prestito.java
@@ -1,8 +1,49 @@
 package it.unisa.biblioteca.model;
 
+import java.time.LocalDate;
+
 public class Prestito {
-    private final Utente utentePrestito;
-    private final Libro libroPrestito;
+    private final Utente utente;
+    private final Libro libro;
+    private final LocalDate dataInizio;
+    private final LocalDate dataFinePrevista; // Ecco la data che cercavi!
 
 
+    ///  ---Costruttore ---///
+    public Prestito(Utente utente, Libro libro, LocalDate dataInizio) {
+        this.utente = utente;
+        this.libro = libro;
+        this.dataInizio = dataInizio;
+
+        // Regola di business: il prestito dura 60 giorni fissi
+        this.dataFinePrevista = dataInizio.plusDays(60);
+    }
+
+    /// --- Getter --- ///
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public LocalDate getDataFinePrevista() {
+        return dataFinePrevista;
+    }
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    /// --- Gestione stato --- ///
+    // Verifica se il prestito è scaduto (utile per colorare di rosso la GUI)
+    public boolean isScaduto() {
+        return LocalDate.now().isAfter(dataFinePrevista);
+    }
+
+    @Override
+    public String toString() {
+        return "Prestito: " + libro.getTitolo() + " scade il " + dataFinePrevista;
+    }
 }
