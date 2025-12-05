5d65e57222252b5cf85b985b1bf9c107812d8b4d
Mattia <mattia.prova0455@gmail.com>
2025-12-05 08:37:32 +0100

    Aggiornata e conclusa la classe di test UtenteTest

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,5 +3,5 @@ var searchData=
   ['aggiungilibroview_0',['AggiungiLibroView',['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_aggiungi_libro_view.html',1,'it::unisa::biblioteca::view']]],
   ['aggiungiprestito_1',['aggiungiPrestito',['../classit_1_1unisa_1_1biblioteca_1_1model_1_1_utente.html#ab208e3765e507f6375920521ff2cbdac',1,'it::unisa::biblioteca::model::Utente']]],
   ['aggiungiprestitoview_2',['AggiungiPrestitoView',['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_aggiungi_prestito_view.html',1,'it::unisa::biblioteca::view']]],
-  ['aggiungiutenteview_3',['AggiungiUtenteView',['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_aggiungi_utente_view.html',1,'it::unisa::biblioteca::view']]]
+  ['aggiungiutenteview_3',['AggiungiUtenteView',['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_aggiungi_utente_view.html',1,'it.unisa.biblioteca.view.AggiungiUtenteView'],['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_aggiungi_utente_view.html#afa6a8764fb4a1bb9822686f446cfa6bd',1,'it.unisa.biblioteca.view.AggiungiUtenteView.AggiungiUtenteView()']]]
 ];
--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['rimuoviprestito_0',['rimuoviPrestito',['../classit_1_1unisa_1_1biblioteca_1_1model_1_1_utente.html#aca1b5729cdeee8564d7da9f1b56613d1',1,'it::unisa::biblioteca::model::Utente']]]
+  ['refresh_0',['refresh',['../classit_1_1unisa_1_1biblioteca_1_1view_1_1_libri_view.html#a41ffe47849144ebffc7c9176da5b88b2',1,'it::unisa::biblioteca::view::LibriView']]],
+  ['rimuoviprestito_1',['rimuoviPrestito',['../classit_1_1unisa_1_1biblioteca_1_1model_1_1_utente.html#aca1b5729cdeee8564d7da9f1b56613d1',1,'it::unisa::biblioteca::model::Utente']]]
 ];
